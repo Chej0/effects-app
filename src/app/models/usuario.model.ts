d08b77c57df7d301557b6d6827ef40eeb4a71071
DESKTOP-KU88QJ5\Skyguard <sergiomendez0705@gmail.com>
2020-04-13 20:12:17 -0600

    primer commit efects

---
+++ src/app/models/usuario.model.ts
@@ -0,0 +1,4 @@
+export class Usuario {
+    // tslint:disable-next-line: variable-name
+    constructor(public id: any, public email: any, public first_name: any, public last_name: any, public avatar: any) {}
+}
